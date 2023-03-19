32d71505a1e005e55e506b520a366bea8765b0a0
JacoboOsorio <zapatanosedon@gmail.com>
2023-03-18 19:59:38 -0500

    New WebAPI Shopping project with new Country entity

---
+++ ShoppingWebAPI/ShoppingWebAPI/Entities/Country.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingWebAPI.Entities
+{
+    public class Country : Entity
+    {
+        [Display(Name = "Pais")] //To show the field with another name
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener maximo {1} caracteres")] //Equals "NVARCHAR()" in SQL
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")] //Equals "NOT NULL" in SQL
+        public string Name { get; set; }
+    }
+}
