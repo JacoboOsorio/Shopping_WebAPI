4357729064487181888161d4822f4606a508024c
JacoboOsorio <zapatanosedon@gmail.com>
2023-03-18 20:57:53 -0500

    New DataBaseContext and ConnectionString created to configure my new database

---
+++ ShoppingWebAPI/ShoppingWebAPI/DAL/DataBaseContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingWebAPI.DAL.Entities;
+
+namespace ShoppingWebAPI.DAL
+{
+    //Here's where i manipulate the database
+    //for me to do whatever i wanna do
+    public class DataBaseContext : DbContext
+    {
+        //Takes an instance from "DbContextOptions" pre-defined
+        //on "EntityFrameworkCore" library, and i will add it a
+        //property called "options" to control the database
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        //Naming the table for the database
+        private DbSet<Country> Countries { get; set; }
+
+        //Creating an index to control duplicated data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
--- ShoppingWebAPI/ShoppingWebAPI/Program.cs
+++ ShoppingWebAPI/ShoppingWebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ShoppingWebAPI.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//"=>": means "Tal que"
+builder.Services.AddDbContext<DataBaseContext>(o =>
+{
+    //Creating a dependency for a sql-kind database
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
