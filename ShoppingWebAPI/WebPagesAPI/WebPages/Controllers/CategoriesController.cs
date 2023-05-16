04ffbe599fb933880fb67b4645a2f7ebe53cd725
Jacobo Osorio <zapatanosedon@gmail.com>
2023-05-16 16:22:52 -0500

    New project WebPages to consume the ShoppingWebAPI. Index action created with its HTML.

---
+++ ShoppingWebAPI/WebPagesAPI/WebPages/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ShoppingWebAPI.DAL.Entities;
+
+namespace WebPages.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+
+        public CategoriesController(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var url = "https://localhost:7067/api/Categories/Get";
+            var json = await _httpClient.CreateClient().GetStringAsync(url);
+            List<Category> categories = JsonConvert.DeserializeObject<List<Category>>(json);
+            return View(categories);
+        }
+    }
+}
---
+++ ShoppingWebAPI/WebPagesAPI/WebPages/Models/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingWebAPI.DAL.Entities
+{
+    public class Entity
+    {
+        //Data annotation: Kind of labels that i give
+        //to my properties or fields
+        [Key] //Primary key
+        //Guid: a hash that changes randomly and
+        //it's not self-incrementing
+        public Guid Id { get; set; }
+
+        //?: Means that properties are nullable
+        //(There's no problem if i don't send any
+        //value or parameter)
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
